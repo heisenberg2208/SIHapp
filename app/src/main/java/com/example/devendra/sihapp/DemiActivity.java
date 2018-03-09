1a8a677fe7cc3f8523c3f13e39ff4c3b923f10cc
heisenberg2208 <devendra.chaudhari07@gmail.com>
2018-03-09 20:58:55 +0530

    Pankaj's part has been successfully implemented

---
+++ app/src/main/java/com/example/devendra/sihapp/DemiActivity.java
@@ -0,0 +1,213 @@
+package com.example.devendra.sihapp;
+
+        import android.content.Context;
+        import android.content.DialogInterface;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.annotation.NonNull;
+        import android.support.v7.app.AlertDialog;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import com.firebase.ui.auth.AuthUI;
+        import com.firebase.ui.auth.IdpResponse;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+        import com.google.firebase.auth.UserProfileChangeRequest;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.firestore.FirebaseFirestore;
+        import com.google.firebase.storage.StorageReference;
+
+        import java.util.Arrays;
+        import java.util.List;
+
+
+public class DemiActivity extends AppCompatActivity {
+
+
+    public static final int RC_SIGN_IN =1;
+
+
+
+
+
+
+    //Auth
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    //Auth Ends here
+    private Button btnSignOut;
+    private TextView tvType;
+
+    private StorageReference storageReference;
+    private FirebaseFirestore firestore;
+    private RecyclerView rcv;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference fireRef;
+
+    LinearLayoutManager linearLayoutManager;
+    List<Crop> cropsList;
+    private CropsListAdapter cropsListAdapter;
+    public static Context mcontext;
+    public static Context getContext()
+    {
+        return mcontext;
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignOut = (Button) findViewById(R.id.btnSignOut);
+        tvType = (TextView)findViewById(R.id.tvType);
+
+        //Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        //Auth ends
+
+
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance().signOut(DemiActivity.this);
+            }
+        });
+        //Auth
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull final FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null)
+                {
+
+
+
+                    if (user.getDisplayName()==null || user.getDisplayName().equals("")) {
+
+                        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(DemiActivity.this);
+                        View mView = layoutInflaterAndroid.inflate(R.layout.pop_layout, null, false);
+                        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(DemiActivity.this);
+                        alertDialogBuilderUserInput.setView(mView);
+
+                        final EditText userInputDialogEditText = (EditText) mView.findViewById(R.id.etPop);
+
+
+                        alertDialogBuilderUserInput
+                                .setCancelable(false)
+                                .setPositiveButton("Set User Name", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        final String input = userInputDialogEditText.getText().toString();
+                                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                                .setDisplayName(input).build();
+                                        FirebaseUser u = firebaseAuth.getCurrentUser();
+                                        u.updateProfile(profileUpdates);
+
+//                                        Toast.makeText(MainActivity.this, input, Toast.LENGTH_SHORT).show();
+
+
+                                    }
+                                });
+
+
+                        AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+                        alertDialogAndroid.show();
+
+
+                    }
+                    Toast.makeText(DemiActivity.this, "Welcome: " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+
+
+                }
+                else
+                {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.PhoneBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()
+                                    ))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+        //Auth ended
+
+        Intent i = getIntent();
+        String type = i.getStringExtra("type");
+
+        Intent in = new Intent(DemiActivity.this , MainActivity.class);
+        startActivity(in);
+        finish();
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+//Auth helping method for AuthStateListener
+
+//Auth ended
+
+
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN)
+        {
+
+            if(resultCode == RESULT_OK)
+            {
+                Toast.makeText(this, "You are signed in", Toast.LENGTH_SHORT).show();
+            }
+            else if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(this, "Sign in cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+
+
+}
+
--- app/src/main/java/com/example/devendra/sihapp/MainActivity.java
+++ app/src/main/java/com/example/devendra/sihapp/MainActivity.java
@@ -1,51 +1,50 @@
 package com.example.devendra.sihapp;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.StorageReference;
 
-import java.util.Arrays;
-import java.util.List;
 
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.design.widget.Snackbar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.util.Log;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import com.bumptech.glide.Glide;
+        import com.firebase.ui.auth.AuthUI;
+        import com.firebase.ui.auth.ErrorCodes;
+        import com.firebase.ui.auth.IdpResponse;
+        import com.firebase.ui.auth.ResultCodes;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.database.ChildEventListener;
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.firestore.DocumentChange;
+        import com.google.firebase.firestore.DocumentSnapshot;
+        import com.google.firebase.firestore.EventListener;
+        import com.google.firebase.firestore.FirebaseFirestore;
+        import com.google.firebase.firestore.FirebaseFirestoreException;
+        import com.google.firebase.firestore.Query;
+        import com.google.firebase.firestore.QuerySnapshot;
+        import com.google.firebase.storage.FirebaseStorage;
+        import com.google.firebase.storage.StorageReference;
+
+
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    public static final int RC_SIGN_IN =1;
-
-
-
-
-
-
-    //Auth
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-    //Auth Ends here
-    private Button btnSignOut;
-    private TextView tvType;
-
     private StorageReference storageReference;
     private FirebaseFirestore firestore;
     private RecyclerView rcv;
@@ -61,150 +60,113 @@ public class MainActivity extends AppCompatActivity {
         return mcontext;
 
     }
-
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSignOut = (Button) findViewById(R.id.btnSignOut);
-        tvType = (TextView)findViewById(R.id.tvType);
 
-        //Auth
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        //Auth ends
+        cropsList=new ArrayList<>();
+        cropsListAdapter = new CropsListAdapter(cropsList);
+        mcontext=getApplicationContext();
 
+        FirebaseAuth auth = FirebaseAuth.getInstance();
 
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firestore = FirebaseFirestore.getInstance();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        rcv =findViewById(R.id.rcv);
+        rcv.setHasFixedSize(true);
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+        rcv.setAdapter(cropsListAdapter);
+        fireRef=firebaseDatabase.getReference();
+
+        firestore.collection("Crop_info").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
-            public void onClick(View v) {
-                AuthUI.getInstance().signOut(MainActivity.this);
-            }
-        });
-        //Auth
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull final FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(user != null)
-                {
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.d("Erroe", e.toString());
+                } else {
+                    fireRef.addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                            String var = dataSnapshot.getValue(String.class);
+                            Toast.makeText(MainActivity.this, var, Toast.LENGTH_SHORT).show();
+                        }
 
-
-
-                        if (user.getDisplayName()==null || user.getDisplayName().equals("")) {
-
-                            LayoutInflater layoutInflaterAndroid = LayoutInflater.from(MainActivity.this);
-                            View mView = layoutInflaterAndroid.inflate(R.layout.pop_layout, null, false);
-                            AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(MainActivity.this);
-                            alertDialogBuilderUserInput.setView(mView);
-
-                            final EditText userInputDialogEditText = (EditText) mView.findViewById(R.id.etPop);
-
-
-                            alertDialogBuilderUserInput
-                                    .setCancelable(false)
-                                    .setPositiveButton("Set User Name", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            final String input = userInputDialogEditText.getText().toString();
-                                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                                    .setDisplayName(input).build();
-                                            FirebaseUser u = firebaseAuth.getCurrentUser();
-                                            u.updateProfile(profileUpdates);
-
-//                                        Toast.makeText(MainActivity.this, input, Toast.LENGTH_SHORT).show();
-
-
-                                        }
-                                    });
-
-
-                            AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
-                            alertDialogAndroid.show();
-
+                        @Override
+                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
                         }
-                        Toast.makeText(MainActivity.this, "Welcome: " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+
+                        @Override
+                        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                    for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                        if (doc.getType() == DocumentChange.Type.ADDED) {
+                            Crop crop = doc.getDocument().toObject(Crop.class);
+                            cropsList.add(crop);
+                            cropsListAdapter.notifyDataSetChanged();
+                            String title = doc.getDocument().getString("Title");
+
+                        }
+                    }
 
 
-                }
-                else
-                {
-                    startActivityForResult(
-                            AuthUI.getInstance()
-                                    .createSignInIntentBuilder()
-                                    .setIsSmartLockEnabled(false)
-                                    .setAvailableProviders(Arrays.asList(
-                                            new AuthUI.IdpConfig.EmailBuilder().build(),
-                                            new AuthUI.IdpConfig.PhoneBuilder().build(),
-                                            new AuthUI.IdpConfig.GoogleBuilder().build()
-                                    ))
-                                    .build(),
-                            RC_SIGN_IN);
                 }
             }
-        };
-        //Auth ended
-
-        Intent i = getIntent();
-        String type = i.getStringExtra("type");
-        tvType.setText(type);
-
-
-
-
+        });
     }
-
-
-
-
-
-
-
-
-
-//Auth helping method for AuthStateListener
-
-//Auth ended
-
-
-
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
-    }
-
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RC_SIGN_IN)
-        {
 
-            if(resultCode == RESULT_OK)
-            {
-                Toast.makeText(this, "You are signed in", Toast.LENGTH_SHORT).show();
-            }
-            else if(resultCode == RESULT_CANCELED)
-            {
-                Toast.makeText(this, "Sign in cancelled", Toast.LENGTH_SHORT).show();
-                finish();
-            }
+                firestore.collection("Crop_info").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                        if(e!=null){
+                            Log.d("Erroe",e.toString());
+                        }
+                        else
+                        {
+                            for(DocumentChange doc: documentSnapshots.getDocumentChanges()){
+                                if(doc.getType()==DocumentChange.Type.ADDED)
+                                {
+                                    Crop crop = doc.getDocument().toObject(Crop.class);
+                                    cropsList.add(crop);
+                                    cropsListAdapter.notifyDataSetChanged();
+                                    String title =doc.getDocument().getString("Title");
+                                    Log.d("Title",title);
+                                }
+                            }
+                        }
+                    }
+                });
+
+
+
         }
+
+    @Override
+    public void onStart() {
+        super.onStart();
 
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    public void onStop() {
+        super.onStop();
+
     }
-
-
-
 }
-
